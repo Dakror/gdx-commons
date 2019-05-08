ba3da584200a720c8a306353d8266c628b84d96d
Dakror <mail@dakror.de>
2019-05-08 21:52:01 +0200

    point to separate file

--- core/src/de/dakror/common/libgdx/math/ConcaveHull.java
+++ core/src/de/dakror/common/libgdx/math/ConcaveHull.java
@@ -22,52 +22,6 @@ import de.dakror.common.libgdx.Pair;
  * With help from https://github.com/detlevn/QGIS-ConcaveHull-Plugin/blob/master/concavehull.py
  */
 public class ConcaveHull {
-
-    public static class Point {
-
-        private final Double x;
-        private final Double y;
-
-        public Point(Double x, Double y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Double getX() {
-            return x;
-        }
-
-        public Double getY() {
-            return y;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + x + " " + y + ")";
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof Point) {
-                if (x.equals(((Point) obj).getX()) && y.equals(((Point) obj).getY())) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public int hashCode() {
-            // http://stackoverflow.com/questions/22826326/good-hashcode-function-for-2d-coordinates
-            // http://www.cs.upc.edu/~alvarez/calculabilitat/enumerabilitat.pdf
-            int tmp = (int) (y + ((x + 1) / 2));
-            return Math.abs((int) (x + (tmp * tmp)));
-        }
-    }
-
     private Double euclideanDistance(Point a, Point b) {
         return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
     }
