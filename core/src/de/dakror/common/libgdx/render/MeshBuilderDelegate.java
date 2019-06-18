608e1ea163dee4dd1b06f33d43cef1b8697a0e8e
Dakror <mail@dakror.de>
2019-06-18 19:39:32 +0200

    fix crash when freeing mesh

--- core/src/de/dakror/common/libgdx/render/MeshBuilderDelegate.java
+++ core/src/de/dakror/common/libgdx/render/MeshBuilderDelegate.java
@@ -278,6 +278,7 @@ public class MeshBuilderDelegate implements SpriteRenderer {
     public void dispose() {
         if (mesh != null) {
             mesh.dispose();
+            mesh = null;
         }
     }
 }
